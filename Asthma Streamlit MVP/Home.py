972b50a8c7cc942cc63b27cfe8d4a968841c0e8c
Rizm10 <Rizwaanrmiah1@hotmail.com>
2025-12-20 19:59:34 +0000

    Started home.py , wrote try handle to import geolocation and page configuration.

---
+++ Asthma Streamlit MVP/Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+try:
+    from streamlit_geolocation import geolocation # pip install streamlit-geolocation
+except Exception:
+    streamlit_geolocation = None
+    st.warning("streamlit-geolocation package not found. Geolocation features will be disabled.")
+from src.openweather_api import fetch_openmeteo_current_weather
+
+st.config.page_title ( page_title = "Asthma Risk Precautions System" , layoit = "centered" )
+st.title ("Asthma Environmental Risk Index")
